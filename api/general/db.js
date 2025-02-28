97a6a1146b022f90301019b8cc0cee9379f77e4e
samuelail <ailemenizzy@gmail.com>
2025-02-27 22:33:41 -0500

    update

--- api/general/db.js
+++ api/general/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     connectionLimit : 10,
     host: process.env.DB_SERVER,
-    port: 8889, //Conditional
+    //port: 8889, //Conditional
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
